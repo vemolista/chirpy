52dc7f114f9906adf8c5889af8b0935cb9bf2be5
vemolista <tomas@tomasvemola.com>
2025-06-16 19:29:44 +0200

    ex 1.11

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ const PORT = ":8080"
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", healthHandler)
 
 	httpServer := http.Server{
 		Handler: serveMux,
@@ -18,4 +19,14 @@ func main() {
 
 	fmt.Printf("Listening on port %v\n", PORT)
 	httpServer.ListenAndServe()
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		fmt.Printf("error writing request body: %v\n", err)
+	}
 }
