4876e1702c1edbb4b4645098a4ae00831066d125
vemolista <tomas@tomasvemola.com>
2025-06-20 22:09:07 +0200

    Remove comments

--- handler_reset_metrics.go
+++ handler_reset_metrics.go
@@ -23,15 +23,4 @@ func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request
 	respondWithJson(w, http.StatusOK, returnValue{
 		Body: "Hits reset to 0",
 	})
-
-	// w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	// w.WriteHeader(http.StatusOK)
-
-	// cfg.fileserverHits.Store(0)
-	// msg := "Hits reset to 0"
-
-	// _, err = w.Write([]byte(msg))
-	// if err != nil {
-	// 	fmt.Printf("error writing request body: %v\n", err)
-	// }
 }
